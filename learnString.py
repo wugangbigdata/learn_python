d8949841374506c9f2b35dae816073160f53d02b
wugangbigdata <850467075@qq.com>
2015-12-19 23:09:38 +0800

    learn more string operation

--- learnString.py
+++ learnString.py
@@ -2,6 +2,7 @@
 #python 默认ASCII码，不支持中文，第一句表示用utf-8字符编码方式
 #这是一个学习字符串操作的实例，字符串是不可改变值的
 #单引号字符串操作
+from math import pi
 from string import Template
 
 singleQuotation = 'wugang love "bigdata" !'
@@ -40,3 +41,18 @@ keyValue['name'] = 'wugang'
 keyValue['prefix'] = 'big'
 shouting = shouting.substitute(keyValue)
 print shouting
+
+#字符串格式化转换类型
+#d,i  带符号十进制  o 不带符号八进制  u  不带符号十进制  x X不带符号十六进制
+#e E科学计数法浮点数   f F十进制浮点数
+#r s字符串
+#宽度和精度  %10.2f   #字段宽10，小数点2位
+#0,+,-和空格  用来对齐和填充
+#0表示用0来填充左边空位
+#-表示左对齐，右侧用空格填充
+#+表示不管正数还是负数都标示出符号
+#空格表示正数前加空格
+format = '%010.2f'%pi
+print format
+format = '%+5d'%10 + '\n' + '%+5d'%-10
+print format
